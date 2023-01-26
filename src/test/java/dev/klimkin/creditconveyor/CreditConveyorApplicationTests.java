76d2e560b5277f4dd1c67727e3b02ea17615e10a
Oleg Klimkin <oleg402@bk.ru>
2023-01-26 14:00:23 +0300

    Initial commit

---
+++ src/test/java/dev/klimkin/creditconveyor/CreditConveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.klimkin.creditconveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditConveyorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
